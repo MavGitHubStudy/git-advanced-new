10e038c352b9c263fa9fb3d10056ebda0a09363a
Andrey Milykh <amilykh@mail.ru>
2023-07-22 22:27:29 +0300

    added comment

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 (function () {
-  document.addEventListener('DOMContentLoaded', function (){
-    document.getElementById('button').addEventListener('click', function(){
-      let text = document.getElementById('text').nodeValue;   
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('button').addEventListener('click', function () {
+      let text = document.getElementById('text').nodeValue;
       alert('Длина строки: ' + text);
+      /* comment */
     });
-  });  
+  });
 })();
